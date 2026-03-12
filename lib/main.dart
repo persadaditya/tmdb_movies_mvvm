c7a0d3af3f59e83ded64db6a55f8b8ee407ee87d
persadaditya <adityaariefpersada@gmail.com>
2026-03-12 23:46:01 +0700

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import 'package:responsive_builder/responsive_builder.dart';
+import 'package:tmdb_movies/app/app.bottomsheets.dart';
+import 'package:tmdb_movies/app/app.dialogs.dart';
+import 'package:tmdb_movies/app/app.locator.dart';
+import 'package:tmdb_movies/app/app.router.dart';
+import 'package:url_strategy/url_strategy.dart';
+import 'package:flutter_animate/flutter_animate.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  setPathUrlStrategy();
+  await setupLocator(stackedRouter: stackedRouter);
+  setupDialogUi();
+  setupBottomSheetUi();
+  runApp(const MainApp());
+}
+
+class MainApp extends StatelessWidget {
+  const MainApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ResponsiveApp(
+      builder: (_) => MaterialApp.router(
+        debugShowCheckedModeBanner: false,
+        routerDelegate: stackedRouter.delegate(),
+        routeInformationParser: stackedRouter.defaultRouteParser(),
+      ),
+    ).animate().fadeIn(
+          delay: const Duration(milliseconds: 50),
+          duration: const Duration(milliseconds: 400),
+        );
+  }
+}
